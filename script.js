a2ca22f910a2787e6ce26ef3370b348aec3d3eff
Harshit Dhoni <harshitdhoni900@gmail.com>
2024-12-16 21:42:15 +0530

    new file added

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ let sum = 0;
 array.forEach(n => sum += n);
 console.log(sum)
 
+const obj = {
+    name : "Harshit",
+    about : {
+        age : 21,
+        profession : "Developer",
+
+    }
+}
+console.log(obj) ;
+
+function fun(){
+    document.write('hello world')
+
+}
+    
+    fun();
+
+let name = ["harshit", "karan", "rohit", "akash", "amit", "sagar"];
+for (friends in name){
+    console.log(friends);
+}   
+
+for (let i = 0; i<=10; i++){
+    document.write(i);
+}
+
+
+
+
+
+
